35121cf26df7b7451728a1588b0a6a4179dde4f6
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-08-24 08:02:30 -0400

    Simple Red Hover added to bodies, body added in place for muscle discesstion >:)

---
+++ muscles.js
@@ -0,0 +1,11 @@
+let frontbody = document.getElementById('FrontBody');
+let backbody = document.getElementById('BackBody')
+
+
+frontbody.addEventListener("load", () => {
+    let doc = frontbody.contentDocument;
+    console.log(doc);
+
+}, false)
+
+
