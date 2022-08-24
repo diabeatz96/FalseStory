cc2b577b4d859b1c5914b13ea7b006fb0d13e6bb
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-08-24 11:00:27 -0400

    Changed body-back.svg and body-front.svg to be more specific with muscle groups, added in muscles.js and fixed issues with body type. Most other issues revolving this has been added. Muscle logic has begun to work.

--- muscles.js
+++ muscles.js
@@ -2,9 +2,45 @@ let frontbody = document.getElementById('FrontBody');
 let backbody = document.getElementById('BackBody')
 
 
+/*
+     The best approach i can think of is iteratively going through each muscle and showcasing the box hover, space might be too hard to cover
+     with the whole layout already there.
+ */
+
+
+/* Added function for creating boxes */
+
+function boxSettings(element, e) {
+    element.setAttribute("id", "infobox");
+    element.style.width = "150px";
+    element.style.height = "150px";
+    element.style.backgroundColor = "white";
+    element.style.position = "absolute";
+    element.style.top = e.clientY + 50 + 'px';
+    element.style.left = e.clientX + 100 + 'px';
+}
+
+
 frontbody.addEventListener("load", () => {
     let doc = frontbody.contentDocument;
-    console.log(doc);
+
+    doc.querySelector("#head").addEventListener("mouseenter", (e) => {
+        console.log("Mouse is clicked!");
+        const newBox = document.createElement("div");
+        boxSettings(newBox, e);
+        document.querySelector(".Bodies").append(newBox);
+    })
+
+    doc.querySelector("#head").addEventListener("mouseleave", () => {
+        const box = document.getElementById("infobox");
+        box.remove();
+    })
+
+    doc.querySelector("#head").addEventListener("click", () => {
+        /*
+             fill in info to add modal window for specifics of clicking functionality
+         */
+    })
 
 }, false)
 
