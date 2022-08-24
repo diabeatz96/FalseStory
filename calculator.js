6bbaac5097ecb5e7a765160b62447ffbf605f746
Affat25 <affat9422@gmail.com>
2022-08-24 18:49:45 -0400

    aboutus updated

--- calculator.js
+++ calculator.js
@@ -1,17 +1,18 @@
+
 let submission = document.querySelector(".submission");
 
-const exercise_ = document.querySelector("#lift");
+const exercise = document.querySelector("#lift");
 const weight_ = document.querySelector("#weight");
 const repetitions_ = document.querySelector("#repetitions");
 const percentage_ = document.querySelector("#percentage");
 
-const weight = parseFloat(weight_.value);
-const repetitions = parseFloat(repetitions_.value);
-const percentage = parseFloat(percentage_.value);
+let weight = parseFloat(weight_.value);
+let repetitions = parseFloat(repetitions_.value);
+let percentage = parseFloat(percentage_.value);
 
 submission.addEventListener("click", () => {
     const screen = document.querySelector("#screen")
-    if(!exercise_.value || !weight || !repetitions || !percentage) {
+    if(!exercise.value || !weight || !repetitions || !percentage) {
         screen.textContent = "Fill All Fields"
     } else {
         /*
